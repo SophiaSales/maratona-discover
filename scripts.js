2d08b87efaba0bbab816a97a1c13819bea5e1863
SophiaSales <sophiabrenda10@gmail.com>
2021-02-09 20:05:22 -0300

    fix: javascripts fixes

--- scripts.js
+++ scripts.js
@@ -14,40 +14,40 @@ const Modal = {
 
     }
 }    
-const transactions = [ 
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021',
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021',
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021',
-    },
-    {
-        id: 4,
-        description: 'App',
-        amount: 200000,
-        date: '23/01/2021',
-    },
-
-]
 
 
 const Transaction = {
-    all: transactions,
+    all: [ 
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021',
+        },
+        {
+            description: 'App',
+            amount: 200000,
+            date: '23/01/2021',
+        },
+    
+    ],
 
     add(transaction){
         Transaction.all.push(transaction)
+        App.reload()
+    },
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
         App.reload()
     },
     incomes () {
@@ -135,6 +135,43 @@ const Utils = {
     }
 
 }
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues(){
+        return{
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    //formatData() {
+      //  console.log('Formatar os dados')
+    //},
+    validateFields(){
+        const {description, amount, date } = Form.getValues()
+        if(description.trim() === "" || 
+        amount.trim() === "" || 
+        date.trim() === ""){
+            throw new Error("Por favor preecha todod os campos")
+        }
+    },
+    submit(event){
+        event.preventDefault()
+        
+    try{
+        Form.validateFields()
+        //Form.formatData()
+
+    }catch(error){
+        alert(error.message)
+    }
+        
+    }
+}
 const App = {
     init() {
         
@@ -151,11 +188,3 @@ const App = {
 }
 
 App.init()
-
-
-Transaction.add({
-    id:39,
-    description : "Alo",
-    amount:200,
-    date:'23/01/2021'
-})
